8929fd953492df6461fc92c37df8eb5d1d82ccea
Piyush Yadav <piyushydv011@gmail.com>
2025-06-03 13:51:39 +0530

    lec 41 done

---
+++ Web lectures/lec41/index.js
@@ -0,0 +1,70 @@
+let a = 10;
+let b = 5;
+
+// a += 10;
+// console.log(a);
+
+// console.log(a**b);
+
+// console.log(a>b);
+
+// console.log("5" == 5);
+// console.log("5" === 5);
+
+// let age = 25;
+// let status1 = (age>18) ? 'I can vote' : 'I cannot vote';
+// console.log(status1);
+
+// let ans = (true && true && true);
+// console.log(ans);
+
+// console.log(false || "Piyush");
+
+// console.log(false || 7 || 87 || 90);
+
+// console.log(2 ^ 5);
+
+// console.log(10 << 1);
+
+// let age = 53;
+// if(age >= 18){
+//     console.log("can vote");
+// }
+// else{
+//     console.log("can not vote")
+// }
+
+// let number = 5;
+
+// if(number == 1){
+//     console.log('A');
+// }
+// else if(number == 2){
+//     console.log('B');
+// }
+// else if(number == 3){
+//     console.log('C');
+// }
+// else if(number == 4){
+//     console.log('D');
+// }
+// else if(number == 5){
+//     console.log('E');
+// }
+// else {
+//     console.log('F');
+// }
+
+let num = 3;
+
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('E');
+}
