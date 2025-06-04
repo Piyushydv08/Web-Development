5feba2a02b3b57feb40527f2fcae312ae9ddeeef
Piyush Yadav <piyushydv011@gmail.com>
2025-06-04 08:33:56 +0530

    lec 42 done

---
+++ Web lectures/lec42/index.js
@@ -0,0 +1,72 @@
+// for(let i=1; i<=10; i++){
+//     console.log("Piyush");
+// }
+
+// let i =1;
+// while(i<=10){
+//     console.log("Piyush")
+//     i++;
+// }
+
+// let i = 1;
+// do{
+//     console.log("piyush");
+//     i++
+// }
+// while(i <=10);
+
+// for(let i=5; i>=0; i--){
+//     console.log(i);
+// }
+
+// for(let i=1; i<=6; i++){
+//     if(i == 4)
+//         break;
+//     else
+//         console.log(i);
+// }
+
+// for(let i=1; i<=6; i++){
+//     if(i == 4)
+//         continue;
+//     else
+//         console.log(i);
+// }
+
+// let firstName = "Piyush";
+// let lastName = "Yadav";
+
+// let name1 = `Piyush
+//  Yadav
+//  This 
+//  is my 
+//  name.`;
+
+// console.log(firstName);
+// console.log(name1);
+
+// let newName = new String("Piyush Yadav");
+// console.log(newName);
+
+let op1 = 'English';
+let op2 = 'Hindi';
+
+// let finalAns = `${op1} + ${op2}`;
+// console.log(finalAns);
+
+// let ans = op1 + op2;
+// console.log(ans);
+
+// console.log(op2.length);
+// console.log(op1.toUpperCase());
+// console.log(op2.toLowerCase());
+
+// let str = "Piyush";
+// console.log(str.substring(2,4));
+
+let sentence ="Hello Jee \"Kaise\" hoo Saare";
+let words = sentence.split(" ");
+console.log(words);
+console.log(words.join(","));
+
+// \ -> backslash told us to treat next element normal
