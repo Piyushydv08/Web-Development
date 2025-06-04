6496b1f615732d28c63818807e15260b5c23bb72
Piyush Yadav <piyushydv011@gmail.com>
2025-06-04 09:10:41 +0530

    lec 43 done

---
+++ Web lectures/lec43/index.js
@@ -0,0 +1,35 @@
+// function sayMyName() {
+//     console.log("Piyush Yadav");
+// }
+// sayMyName();
+
+// function printCounting(){
+//     for(let i=1; i<=50; i++){
+//         console.log(i)
+//     }
+// }
+// printCounting();
+
+// function getAverage(num1, num2){
+//     let avg = (num1 + num2)/2;
+//     console.log("Average : ", avg);
+// }
+// getAverage(2,80);
+
+// function getSum(a, b, c){
+//     let sum = a+b+c;
+//     return sum;
+// }
+// let ans = getSum(1, 2, 3);
+// console.log("Printing Sum : ", ans);
+
+// let getMultiplication = function(a,b) {
+//     return a*b;
+// }
+// console.log(getMultiplication(2,10));
+
+//-----Arrow Functions----
+let getExp = (x,y) => {
+    return x**y;
+}
+console.log(getExp(2,10));
