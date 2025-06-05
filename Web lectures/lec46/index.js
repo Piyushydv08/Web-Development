30cb63b78564e09fe794e910fd8a834fe0a88500
Piyush Yadav <piyushydv011@gmail.com>
2025-06-05 09:43:57 +0530

    lec 46 done

---
+++ Web lectures/lec46/index.js
@@ -0,0 +1,17 @@
+{
+    var height = 180;
+}
+
+console.log(height);
+// We can access variable outside block only variable which is created using var if we have used let or const then we can not access
+
+//If we created a variable inside a function then we can not access it outside the function
+
+//Global vraiables can be access anywhere
+
+
+console.log(marks);
+console.log("Piyush");
+console.log("Yadav");
+const marks = 100;
+//line 13 to 16 is called temporal dead zone
