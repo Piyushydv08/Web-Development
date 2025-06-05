aeb448e33c1d92043eb6eb2f1c58db924ba9c98f
Piyush Yadav <piyushydv011@gmail.com>
2025-06-05 09:27:46 +0530

    lec 45 done

---
+++ Web lectures/lec45/index.js
@@ -0,0 +1,61 @@
+// console.log(age);
+// const age = 25;
+// //Get error
+
+// console.log(age);
+// var age = 25;
+// //will get undefined
+
+// sayMyName("Piyush");
+// function sayMyName(name){
+//     console.log(name);
+// }
+// //works fine
+
+// sayHello();
+// let sayHello = function() {
+//     console.log("Hello Jii, Kaise ho");
+// }
+// // function hoisting will not work on function expression
+
+// const object1 = new Human();
+// class Human {
+
+// }
+// // This will give error
+
+// function greetMe(greet, fullName) {
+//     console.log("Hello ", fullName)
+//     greet();
+// }
+
+// function greet() {
+//     console.log("Greetings for the day");
+// }
+
+// greetMe(greet, "Piyush");
+
+// function solve(number) {
+//     return function(number) {
+//         return number*number;
+//     }
+// }
+// let ans = solve(5);
+// let finalAns = ans(10);
+// console.log(finalAns);
+
+// const arr = [
+//     function(a,b) {
+//         return a+b;
+//     },
+//     function(a,b) {
+//         return a-b;
+//     },
+//     function(a,b){
+//         return a*b;
+//     }
+// ];
+// let first = arr[2];
+// let ans = first(5,10);
+// console.log(ans);
+
