aa4dd43db61da0837b988184535fcc5803b8f9dc
Piyush Yadav <piyushydv011@gmail.com>
2025-06-06 10:00:57 +0530

    lec 48 done

---
+++ Web lectures/lec48/index.js
@@ -0,0 +1,15 @@
+// console.log(Math.PI);
+// console.log(Math.max(60,30,600, 712, 89));
+// console.log(Math.min(30,60,19,500,689));
+// console.log(Math.round(1.4));
+// console.log(Math.floor(1.4)); //number before given number
+// console.log(Math.ceil(1.4)); //number after given number
+// console.log(Math.abs(-5)); //always give positive number
+// console.log(Math.pow(2,10));
+
+let curr = new Date();
+let date = new Date('June 20 2004 07:15');
+console.log(curr);
+
+console.log(date.getFullYear());
+console.log(date.setFullYear(2005));
